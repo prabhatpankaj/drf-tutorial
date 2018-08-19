6bd92a63d152362c9ccf2734c7f60b4d4e22c172
prabhat <prabhatiitbhu@gmail.com>
2018-08-19 08:53:28 +0530

    tutorial-1.7

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,10 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
